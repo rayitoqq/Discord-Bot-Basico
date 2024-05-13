2824b7def033408f5ebf67ab873fdeb0ebf52e6b
RyayitoqqDev <142483839+rayitoqq@users.noreply.github.com>
2024-05-13 06:01:40 -0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,35 +1,16 @@
+# Importações
 import discord
-from discord import app_commands
-import random
+from discord.ext import commands
 
-id_del_servidor =  # ID de tu servidor
+# Intents e criação do bot
+intents = discord.Intents.all() # Ativa todas as intents
+bot = commands.Bot('!r', intents=intents)
 
-class Cliente(discord.Client):
-    def __init__(self):
-        super().__init__(intents=discord.Intents.default())
-        self.sincronizado = False  # Usamos esto para evitar que el bot sincronice los comandos más de una vez
 
-    async def on_ready(self):
-        await self.wait_until_ready()
-        if not self.sincronizado:  # Verificar si los comandos slash han sido sincronizados
-            await arbol.sincronizar(guild=discord.Object(id=id_del_servidor))  # También puedes dejar en blanco el ID del servidor para aplicarlo a todos los servidores, pero esto tomará entre 1 y 24 horas para que funcione.
-            self.sincronizado = True
-        print(f"Conectado como {self.user}.")
+#Comando "!rhola"
+@bot.command(aliases=["hola"])
+async def olá(ctx): # Crear la función asincrónica
+    autor = ctx.author.mention # Mencion a ti
+    await ctx.send(f'ola, {autor}!') # Envia uma mensage
 
-cliente_discord = Cliente()
-arbol = app_commands.CommandTree(cliente_discord)
-
-@arbol.comando(guild=discord.Object(id=id_del_servidor), nombre='github', descripción='Perfil de GitHub')  # Comando específico para tu servidor
-async def slash2(interacción: discord.Interacción):
-    await interacción.respuesta.enviar_mensaje(f"https://github.com/rayitoqq", efímero=False)
-
-@arbol.comando(guild=discord.Object(id=id_del_servidor), nombre='hola', descripción='Un lindo mensaje solo para ti')  # Comando específico para tu servidor
-async def slash4(interacción: discord.Interacción):
-    await interacción.respuesta.enviar_mensaje(f"¡Hola, bienvenido/a a mi servidor de pruebas! :)", efímero=True)
-
-@arbol.comando(guild=discord.Object(id=id_del_servidor), nombre='dado', descripción='Tira un dado de 6 caras')  # Comando específico para tu servidor
-async def slash3(interacción: discord.Interacción):
-    número = random.randint(1, 6)
-    await interacción.respuesta.enviar_mensaje(f"Número {número}", efímero=False)
-
-cliente_discord.run('pon aquí tu token')
+bot.run('Tu Token') # bot
