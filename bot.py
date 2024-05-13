368b296618bf294599e37fe26abb50decf3e2897
RyayitoqqDev <142483839+rayitoqq@users.noreply.github.com>
2024-05-13 01:56:24 -0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,36 +2,34 @@ import discord
 from discord import app_commands
 import random
 
-id_do_servidor =  #id do seu servidor
+id_del_servidor =  # ID de tu servidor
 
-class client(discord.Client):
+class Cliente(discord.Client):
     def __init__(self):
         super().__init__(intents=discord.Intents.default())
-        self.synced = False #Nós usamos isso para o bot não sincronizar os comandos mais de uma vez
+        self.sincronizado = False  # Usamos esto para evitar que el bot sincronice los comandos más de una vez
 
     async def on_ready(self):
         await self.wait_until_ready()
-        if not self.synced: #Checar se os comandos slash foram sincronizados 
-            await tree.sync(guild = discord.Object(id=id_do_servidor)) # Você também pode deixar o id do servidor em branco para aplicar em todos servidores, mas isso fará com que demore de 1~24 horas para funcionar.
-            self.synced = True
-        print(f"Entramos como {self.user}.")
+        if not self.sincronizado:  # Verificar si los comandos slash han sido sincronizados
+            await arbol.sincronizar(guild=discord.Object(id=id_del_servidor))  # También puedes dejar en blanco el ID del servidor para aplicarlo a todos los servidores, pero esto tomará entre 1 y 24 horas para que funcione.
+            self.sincronizado = True
+        print(f"Conectado como {self.user}.")
 
-aclient = client()
-tree = app_commands.CommandTree(aclient)
+cliente_discord = Cliente()
+arbol = app_commands.CommandTree(cliente_discord)
 
-@tree.command(guild = discord.Object(id=id_do_servidor), name = 'github', description='Perfil do Github') #Comando específico para seu servidor
-async def slash2(interaction: discord.Interaction):
-    await interaction.response.send_message(f"https://github.com/viictorvinicius", ephemeral = False) 
+@arbol.comando(guild=discord.Object(id=id_del_servidor), nombre='github', descripción='Perfil de GitHub')  # Comando específico para tu servidor
+async def slash2(interacción: discord.Interacción):
+    await interacción.respuesta.enviar_mensaje(f"https://github.com/rayitoqq", efímero=False)
 
-@tree.command(guild = discord.Object(id=id_do_servidor), name = 'oi', description='Uma linda mensagem somente para você') #Comando específico para seu servidor
-async def slash4(interaction: discord.Interaction):
-    await interaction.response.send_message(f"Olá, seja muito bem vindo(a) a meu servidor de testes :) ", ephemeral = True)   
+@arbol.comando(guild=discord.Object(id=id_del_servidor), nombre='hola', descripción='Un lindo mensaje solo para ti')  # Comando específico para tu servidor
+async def slash4(interacción: discord.Interacción):
+    await interacción.respuesta.enviar_mensaje(f"¡Hola, bienvenido/a a mi servidor de pruebas! :)", efímero=True)
 
-@tree.command(guild = discord.Object(id=id_do_servidor), name = 'dado', description='Joga um dado de 6 lados') #Comando específico para seu servidor
-async def slash3(interaction: discord.Interaction):
-    numero = random.randint(1, 6)
-    await interaction.response.send_message(f"Número {numero}", ephemeral = False)
+@arbol.comando(guild=discord.Object(id=id_del_servidor), nombre='dado', descripción='Tira un dado de 6 caras')  # Comando específico para tu servidor
+async def slash3(interacción: discord.Interacción):
+    número = random.randint(1, 6)
+    await interacción.respuesta.enviar_mensaje(f"Número {número}", efímero=False)
 
-  
-
-aclient.run('coloque seu token aqui')
+cliente_discord.run('pon aquí tu token')
